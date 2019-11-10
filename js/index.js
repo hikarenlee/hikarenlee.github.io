269e6510ad267dc5dd3cf938befcab9c128161a5
brimwd <brimwd@gmail.com>
2019-11-10 11:19:06 -0800

    add dynamic load js

---
+++ js/index.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+  var targetPage;
+  var targetPageLoaded;
+
+  $('a.dynamicLoad').on('click', function(event){
+    // stop the link from firing
+    event.preventDefault();
+
+    // assign vars
+    targetPage = $(event.target).closest('a').attr('href');
+
+    // will come back from the ajax call as TRUE if succeeded or FALSE if failed
+    loadPage(targetPage, 1);
+  });
+});
+
+async function loadPage (targetPage, attempts) {
+  try {
+    let targetPageLoaded = await getPage(targetPage);
+    if (targetPageLoaded) return;
+
+    // failed once
+    if (attempts === 1) loadPage(targetPage, 2);
+    else { // failed twice
+      // redirect to selected page
+      location.assign(targetPage+'#dynamicLoadFailed');
+      // TODO: AFTER REDIRECT handle hash and remove with `location.hash = ''`;
+    }
+  } catch(err) {
+    console.log(err);
+    location.assign(targetPage+'#asyncError');
+    // TODO: AFTER REDIRECT handle hash and remove with `location.hash = ''`;
+  }
+}
+
+function getPage (pageName) {
+  var pageLoaded = undefined;
+
+  // retrieve page from server
+  $.ajax({
+    url: pageName,
+    dataType: 'json',
+    success: function(response) {
+      window.openPage = pageName;
+      // TODO: Add support for deep-linking?
+
+      // renders the new content
+      renderPage(response);
+      pageLoaded = true;
+    },
+    error: function(xhr, status, error) {
+      console.log(xhr, status, error);
+      pageLoaded = false;
+    },
+    type: 'GET'
+  });
+
+  // inform loadPage function
+  return pageLoaded;
+}
+
+function renderPage (pageHTML) {
+  // minimize nav
+  // create content section
+  // populate content section
+
+}
