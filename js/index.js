d915a51d26fa187d551d6ebdb75cd11e3fe503aa
brimwd <brimwd@gmail.com>
2019-11-10 14:00:19 -0800

    simplifies ajax

--- js/index.js
+++ js/index.js
@@ -10,42 +10,26 @@ $(document).ready(function() {
     targetPage = $(event.target).closest('a').attr('href');
 
     // will come back from the ajax call as TRUE if succeeded or FALSE if failed
-    loadPage(targetPage, 1);
+    getPage(targetPage, 1);
   });
 });
 
-async function loadPage (targetPage, attempts) {
-  try {
-    let targetPageLoaded = await getPage(targetPage);
-    if (targetPageLoaded) return;
-
-    // failed once
-    if (attempts === 1) loadPage(targetPage, 2);
-    else { // failed twice
-      // redirect to selected page
-      location.assign(targetPage+'#dynamicLoadFailed');
-    }
-  } catch(err) {
-    console.log(err);
-    location.assign(targetPage+'#asyncError');
-  }
-}
-
-function getPage (pageName) {
+function getPage (pageName, attempts) {
   // retrieve page from server
   $.ajax({
     url: pageName,
     success: function(response) {
       window.openPage = pageName;
-      // TODO: Add support for deep-linking?
-
       // renders the new content
       renderPage(response);
-      return true;
+
+      // TODO: Add support for deep-linking?
+
     },
     error: function(xhr, status, error) {
       console.log(xhr, status, error);
-      return false;
+      if (attempts === 1) getPage(pageName, 2); // try again
+      else location.assign(targetPage+'#dynamicLoadFailed'); // failed twice - redirect to selected page
     },
     type: 'GET'
   });
